2c0bc8de83d07e6798303ddc78611fd0d9ae151b
ارشیا صمیمی فر <A.Samimifar@tav.local>
2026-01-05 17:15:19 +0330

    f

---
+++ home_module/static/home_module/js/main.js
@@ -0,0 +1,9 @@
+function showMenu() {
+    var menu = document.getElementById("menu");
+
+    if (menu.classList.contains("menu-open")) {
+        menu.classList.remove("menu-open");
+    } else {
+        menu.classList.add("menu-open");
+    }
+}
